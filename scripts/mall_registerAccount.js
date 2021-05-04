b383710989a685e1e086ce574e8b9965c90a3e67
Rmit Dev <s3836276@rmit.edu.vn>
2021-05-05 01:22:45 +0700

    add checks for register

--- scripts/mall_registerAccount.js
+++ scripts/mall_registerAccount.js
@@ -65,3 +65,78 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+// check phone number
+btn = document.querySelector("#submitBtn");
+        btn.addEventListener("click", function (ev){
+        // Check any 10 digits
+        phone_pattern = /^([0-9]{1}[-|.| ]?){9,11}$/;
+                        /*********backup***********
+                        // Check phone number with "-"
+                        // phone_pattern_2 = /^(([0-9]+\W){8,10})[0-9]$/;
+                        // phone_pattern_3 = /^([0-9]+\W)([0-9]+\W)([0-9]{3,5})$/;
+                        *********backup***********/
+        value = document.querySelector("#phone").value;
+
+        if (!phone_pattern.test(value)){
+        /*********testing************
+        //     alert("Valid phone number");
+        // } else {
+        **********testing***********/
+            alert("Invalid phone number");
+        }
+        });
+
+
+// Check password
+btn = document.querySelector("#submitBtn");
+      btn.addEventListener("click", function (ev){
+        //   Check password pattern
+          password_pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/
+        var password = document.querySelector("#password").value,
+            confirmPassword = document.querySelector("#passwordRe").value;
+            //check password strength
+            if (!password_pattern.test(password)){
+            /*********testing************    
+            //     alert("Valid password.");}
+            // else{
+            */
+                alert("Password must be 8-20 charaters with at least 1 upper and lower case and atleast 1 special character and no space")
+            };
+            //confirm password
+            if (password == ""){
+                alert("Password cannot be empty.");
+            }
+            else if(password != confirmPassword){
+                alert("Password didn't match.")
+            }
+      });
+// check email
+btn = document.querySelector("#submitBtn");
+      btn.addEventListener("click", function (ev){
+          //regular expression
+          email_pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        value = document.querySelector("#emailAddress").value;
+        if (!email_pattern.test(value)){
+        /**********testing***********
+        //     alert("Valid Email address.");
+        // }
+        //else{
+            **********testing***********/
+            alert("Invalid Email address.");
+        }
+
+        })
+//check name, city, address
+btn = document.querySelector("#submitBtn");
+        brn.addEventListener("click", function (ev){
+            name_pattern = /^[\D]{3,}$/;
+            value = document.querySelector("#firstName").value;
+            if(name_pattern.test(value)){
+                alert("Valid")                
+            }
+            else{
+                alert("Invalid")
+            }
+            
+        })
