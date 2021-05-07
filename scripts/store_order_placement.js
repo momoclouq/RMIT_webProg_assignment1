0477fd09f31b1f85f6a8e1f5ed495f2fb4b0dd6b
Dat Nguyen <s3870837@rmit.edu.vn>
2021-05-08 00:49:36 +0700

    Restyle orderplacement page + implemented js

--- scripts/store_order_placement.js
+++ scripts/store_order_placement.js
@@ -5,6 +5,7 @@ product2TotalPrice = Number.parseInt(product2TotalPrice, 10);
 let totalCost = product1TotalPrice + product2TotalPrice;
 let totalPriceElement = document.querySelector(".priceText");
 let totalNumberHeading = document.querySelector(".title");
+
 function displayCart(){
     let cartItems = sessionStorage.getItem("productsInCart");
     cartItems = JSON.parse(cartItems);
@@ -17,23 +18,42 @@ function displayCart(){
         Object.values(cartItems).map(item => {
           
             productList.innerHTML += `
-            <li class="product">
-                <div class="prodImg"><img src="../../resources/images/Product Image/${item.tag}.jpeg" alt="${item.name}"></div>
+            <li class="product ${item.tag}_cart">
+                <div class="prodImg"><img src="../../resources/images/Product Image/${item.tag}.jpeg" alt="product1 on display"></div>
                 <div class="prodContent">
                     <div class="prodDesc">
-                        <div class="prodName"><a href="product/cate1prod1.html">${item.name}</a></div>
-                        <div class="prodPrice">$${item.price}.00</div>
+                        <div class="prodName">
+                            <h3>${item.name}</h3>    
+                        </div>
+                        <div class="action_button">
+                            <p class="action_button_remove ${item.tag}_remove">Remove</p>
+                        </div>
                     </div>
-                    <div class="prodQuanity">
-                        <button>Remove</button>
-                        <label for="quanity">Quanity <input type="number" id="quanity" name="quanity" value="${item.inCart}"></label>
+                    <div class="prodDetails">
+                        <div class="productPrice">
+                            $${item.price}.00
+                        </div>
+                        <div class="productQuantity">
+                            <ion-icon class="increaseQuantity" name="add-outline"></ion-icon>
+                            <input type="tel" id="quanity${item.tag}" name="quanity" maxlength="10" value="${item.inCart}">
+                            <ion-icon class="decreaseQuantity" name="remove-outline"></ion-icon>
+                        </div>
+                                    
                     </div>
                 </div>
-            </li>
+                </li>
             `
         })
 
-        totalPriceElement.innerHTML += `$${totalCost}.00`;
+        
+        if(product1TotalPrice && product2TotalPrice){
+            totalPriceElement.innerHTML += `$${totalCost}.00`;
+        } else if (product1TotalPrice) {
+            totalPriceElement.innerHTML += `$${product1TotalPrice}.00`;
+        } else if(product2TotalPrice) {
+            totalPriceElement.innerHTML += `$${product2TotalPrice}.00`;
+        }
+        
 
     }
 }
