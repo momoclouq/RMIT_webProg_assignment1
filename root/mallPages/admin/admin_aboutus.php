61649601c0686e85b88f22d13a070a572b5c8d38
Dat Nguyen <s3870837@rmit.edu.vn>
2021-06-01 22:07:07 +0700

    Implement About Us for Admin + Restyle

--- root/mallPages/admin/admin_aboutus.php
+++ root/mallPages/admin/admin_aboutus.php
@@ -10,16 +10,39 @@
     $source_link = $_SERVER["DOCUMENT_ROOT"] . "/../files/common_pages/aboutus.json";
     $source_str = file_get_contents($source_link);
     $source = json_decode($source_str);
-
+    $link = $_SERVER["DOCUMENT_ROOT"] . "/resources/images/profilePic";
     //process the change_request
     if($_POST['change_request'] == "1"){
+       
         foreach($source as $name => $image_link){
-            $path_separated = explode("/", $image_link);
-            $path_separated[count($path_separated)-1] = $_POST[$name];
-            $path = implode("/",$path_separated);
-
-            $source->$name = $path;
-        }
+            if(isset($_FILES[$name])){
+                
+                $errors= array();
+                $file_name = $_FILES[$name]['name'];
+                $file_size =$_FILES[$name]['size'];
+                $file_tmp =$_FILES[$name]['tmp_name'];
+                $file_type=$_FILES[$name]['type'];
+                $file_ext=strtolower(end(explode('.',$_FILES[$name]['name'])));
+                    
+                $extensions= array("jpeg","jpg","png");
+                    
+                if(in_array($file_ext,$extensions)=== false){
+                    $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+                }
+                    
+                if($file_size > 2097152){
+                    $errors[]='File size must be excately 2 MB';
+                }
+                    
+                if(empty($errors)==true){
+                    move_uploaded_file($file_tmp,$link."/".$file_name);
+                    $path_separated = explode("/", $image_link);
+                    $path_separated[count($path_separated)-1] = $file_name;
+                    $path = implode("/",$path_separated);
+                    $source->$name = $path;
+                }
+            }
+        }       
     }
 
     $newSource = json_encode($source);
@@ -33,12 +56,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="admin dashboard">
     <meta name="author" content="Team Developers">
-    <title>Edit Developers profile pic</title>
+    <title>Edit Developers Profile Picture</title>
 
     <link rel="stylesheet" href="/style/mall/common.css">
     <link rel="stylesheet" href="/style/cookie-consent/cookie-consent.css">
     <link rel="stylesheet" href="/style/mall/admin/dashboard.css">
+    <link rel="stylesheet" href="/style/mall/admin/adminAboutUs.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
+
+
 </head>
 
 <body>
@@ -48,36 +74,36 @@
     ?>
 
     <main>
-        <div><a href="admin_dashboard.php">Back to dashboard</a></div>
-        <h1 class="dashboard_title">Adjust developer's profile pics</h1>
+        <div class="back_btn"><a href="admin_dashboard.php">Back to dashboard</a></div>
+        <h1 class="dashboard_title">Change the profile picture</h1>
         <?php 
-            if($_POST['change_request'] == "1") echo "<h2 class=\"status_success\">Profile pics updated</h2>";
+            if($_POST['change_request'] == "1") echo "<h2 class=\"status_success\">Profile picture have been updated</h2>";
         ?>
         <br>
     <!--create form from source data-->
-        <h2>To update the profile pic, please add the pic into this directory first:</h2>
-        <h3>/resources/image/profilePic/</h3>
-        <form action="" method="POST">
-            <?php
-                foreach($source as $name => $image_link){
-                    $path_separated = explode("/", $image_link);
-                    $filename = $path_separated[count($path_separated)-1];
-
-                    $output = <<<"HTML"
-                        <section>
-                            <h2>Developer: $name</h2>
-                            <label for=$name>Enter new filename</label>
-                            <input type="text" id=$name value=$filename name=$name>
-                            <br>
-                            <img class="profile_pic" src=$image_link alt=$name>
-                        </section>
-                    HTML;
-                    echo $output;
-                }
-            ?>
+        <form action="" method="POST" enctype="multipart/form-data">
+            <div class="change_container">
+                <?php
+                    foreach($source as $name => $image_link){
+                        $path_separated = explode("/", $image_link);
+                        $filename = $path_separated[count($path_separated)-1];
+                        $format_name = ucfirst($name);
+                        $output = <<<"HTML"
+                            <section class="individual">
+                                <h2>Developer: $format_name</h2>
+                                <input class="image_upload" type="file" id=$name value=$filename name=$name>
+                                <br>
+                                <img class="profile_pic" src=$image_link alt=$name>
+                            </section>
+                        HTML;
+                        echo $output;
+                    }
+                ?>
+            </div>
+            
             
             <input type="hidden" name="change_request" value="1">
-            <button type="submit">Submit changes</button>
+            <button class="submit_btn" type="submit">Submit changes</button>
         </form>
 
          
